b9bcd1cbff2b9e45b4c43e4d0bd457af6aa59eca
Braden <fish0859@gmail.com>
2020-08-19 15:05:39 -0600

    file structor for fullstack application

---
+++ client/src/components/LitterForm.jsx
@@ -0,0 +1,266 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import PuppyForm from "./PuppyForm";
+import "../style/LitterForm/LitterForm.css";
+//todo paw print as icon/on home page
+export default function LitterForm() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (litterData) => {
+    litterData.puppies = puppies;
+    console.log("litter submit", litterData);
+  };
+  const [puppies, setPuppies] = useState([
+    {
+      id: 1,
+      address: "2020 S 5040 E, South Jordan ut 84402",
+      akcRegistered: false,
+      commission: 200,
+      fees: 80,
+      firstName: "Jack",
+      sex: "M",
+      lastName: "Barry",
+      listed: false,
+      markings: "Chocolate, Dapple",
+      microchipId: 3020384,
+      name: "Red",
+      paymentType: "Cash",
+      paymentValue: 300,
+      phone: "801-391-3949",
+      price: 500,
+      sellerPayment: 30,
+    },
+  ]);
+  const [newPuppyId, setNewPuppyId] = useState(0);
+  const [puppyForm, setPuppyForm] = useState(false);
+  const [editingPuppy, setEditingPuppy] = useState(false);
+  // todo add in error messages (all at top?)
+  const [litterFormData, setLitterFormData] = useState({
+    expectedDate: expectedDateHandler("breedDate", 63),
+    newHomeDate: "",
+  });
+  console.log({ litterFormData });
+  return puppyForm ? (
+    <PuppyForm
+      newPuppyId={newPuppyId}
+      puppies={puppies}
+      setPuppies={setPuppies}
+      setPuppyForm={setPuppyForm}
+      RemovePuppy={removePuppyHandler}
+      editingPuppy={editingPuppy}
+      setEditingPuppy={setEditingPuppy}
+      cancel={() => {
+        setEditingPuppy(false);
+        setPuppyForm(false);
+      }}
+    />
+  ) : (
+    <div className="litter-form">
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="litter-info">
+          <div className="row">
+            <label>
+              Owner's Name:{" "}
+              <input
+                name="ownerName"
+                placeholder="Owner's Name"
+                onChange={handleFormChange}
+                defaultValue={litterFormData.ownerName}
+                ref={register}
+              />
+            </label>
+            {errors.dam && <span>this field is required:</span>}
+            <label>
+              Dam:{" "}
+              <input
+                name="dam"
+                placeholder="Dam"
+                type="text"
+                list="dams"
+                onChange={handleFormChange}
+                defaultValue={litterFormData.dam}
+                ref={register({ required: false })}
+              />
+              {
+                //todo datalist populate from past entries
+              }
+              <datalist id="dams">
+                <option>Rose</option>
+                <option>Sadie</option>
+                <option>Athena</option>
+                <option>Darby</option>
+              </datalist>
+            </label>
+            {errors.sire && <span>this field is required:</span>}
+            <label>
+              Sire:{" "}
+              <input
+                name="sire"
+                placeholder="Sire"
+                type="text"
+                list="sires"
+                onChange={handleFormChange}
+                defaultValue={litterFormData.sire}
+                ref={register({ required: false })}
+              />
+              <datalist id="sires">
+                <option>Rodeo</option>
+                <option>Cowboy</option>
+                <option>Birch</option>
+                <option>Rosco</option>
+              </datalist>
+            </label>
+          </div>
+          <div className="row">
+            {errors.litterDate && <span>this field is required:</span>}
+            {/* //todo date format/validation MM/DD/YY  */}
+            <div className="date">
+              <label>
+                Breed Date:{" "}
+                <input
+                  name="breedDate"
+                  type="date"
+                  onChange={handleFormChange}
+                  defaultValue={litterFormData.breedDate}
+                  ref={register({ required: false })}
+                />
+              </label>
+              <p>
+                Expected Date:{" "}
+                {expectedDateHandler("breedDate", 63, "expectedDate")}
+              </p>
+            </div>
+            <div className="date">
+              <label>
+                Litter Date:{" "}
+                <input
+                  name="litterDate"
+                  type="date"
+                  onChange={handleFormChange}
+                  defaultValue={litterFormData.litterDate}
+                  ref={register({ required: false })}
+                />
+              </label>
+              <p>New Home: {expectedDateHandler("litterDate", 56)}</p>
+            </div>
+          </div>
+        </div>
+        <table className="puppies">
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>sex</th>
+              <th>markings</th>
+              <th>firstName</th>
+              <th>lastName</th>
+              <th>phone</th>
+              <th>price</th>
+              <th>paymentValue</th>
+              <th>paymentType</th>
+              <th>fees</th>
+              <th>sellerPayment</th>
+              <th>commission</th>
+              <th>AKC</th>
+              <th>listed</th>
+            </tr>
+          </thead>
+          <tbody>
+            {puppies
+              ?.sort((a, b) => a.id - b.id)
+              .map(
+                ({
+                  id,
+                  sex,
+                  address,
+                  akcRegistered,
+                  commission,
+                  fees,
+                  firstName,
+                  lastName,
+                  listed,
+                  markings,
+                  microchipId,
+                  name,
+                  phone,
+                  paymentType,
+                  paymentValue,
+                  price,
+                  sellerPayment,
+                }) => (
+                  <tr className="puppy">
+                    <td>{id}</td>
+                    <td>{name}</td>
+                    <td>{sex}</td>
+                    <td>{markings}</td>
+                    <td>{firstName}</td>
+                    <td>{lastName}</td>
+                    <td>{phone}</td>
+                    <td>{price}</td>
+                    <td>{paymentValue}</td>
+                    <td>{paymentType}</td>
+                    <td>{fees}</td>
+                    <td>{sellerPayment}</td>
+                    <td>{commission}</td>
+                    <td>{akcRegistered ? "Yes" : "No"}</td>
+                    <td>{listed ? "Yes" : "No"}</td>
+                    <button onClick={() => editPuppyHandler(id)}>Edit</button>
+                  </tr>
+                )
+              )}
+          </tbody>
+        </table>
+        <button
+          onClick={() => {
+            setNewPuppyId(newPuppyId + 1);
+            setPuppyForm(true);
+          }}
+        >
+          Add Puppy
+        </button>
+        <input type="submit" value="Submit Litter" />
+      </form>
+      {/* <PuppyForm
+        newPuppyId={newPuppyId}
+        puppies={puppies}
+        setPuppies={setPuppies}
+        setPuppyForm={setPuppyForm}
+        RemovePuppy={removePuppyHandler}
+        editingPuppy={editingPuppy}
+        setEditingPuppy={setEditingPuppy}
+        cancel={() => {
+          setEditingPuppy(false);
+          setPuppyForm(false);
+        }}
+      /> */}
+    </div>
+  );
+
+  function expectedDateHandler(dateFrom, days, dataField) {
+    if (watch(dateFrom)) {
+      const baseDate = watch(dateFrom).split("-");
+      let date = new Date(baseDate[0], --baseDate[1], baseDate[2]);
+      let expectedDate = new Date();
+      expectedDate.setDate(date.getDate() + days);
+      expectedDate = expectedDate.toISOString().slice(0, 10);
+      // console.log({ baseDate, date, expectedDate });
+      return expectedDate;
+    }
+    return "N/A";
+  }
+  function editPuppyHandler(id) {
+    setEditingPuppy(puppies.find((puppy) => puppy.id === id));
+    setPuppyForm(true);
+  }
+  function removePuppyHandler(id) {
+    // todo shift id's of other puppies
+    console.log("REMOVE", id);
+    setPuppies(puppies.filter((puppy) => puppy.id !== id));
+  }
+  function handleFormChange(evt) {
+    setLitterFormData({
+      ...litterFormData,
+      [evt.target.name]: evt.target.value,
+    });
+  }
+}
---
+++ client/src/components/PuppyForm.jsx
@@ -0,0 +1,243 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function NewPuppy({
+  newPuppyId,
+  puppies,
+  setPuppies,
+  setPuppyForm,
+  editingPuppy,
+  setEditingPuppy,
+  RemovePuppy,
+  cancel,
+}) {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (puppyData) => {
+    puppyData.commission = CommissionRate();
+    puppyData.id = editingPuppy ? editingPuppy.id : newPuppyId;
+    if (editingPuppy) {
+      console.log("EDIT PUPPY");
+      RemovePuppy(editingPuppy.id);
+      setEditingPuppy(false);
+    }
+    setPuppies([...puppies, puppyData]);
+    setPuppyForm(false);
+  };
+  const {
+    id,
+    address,
+    akcRegistered,
+    commission,
+    fees,
+    firstName,
+    sex,
+    lastName,
+    listed,
+    markings,
+    microchipId,
+    name,
+    paymentType,
+    paymentValue,
+    phone,
+    price,
+    sellerPayment,
+  } = editingPuppy;
+  return (
+    <form className="puppy-form" onSubmit={handleSubmit(onSubmit)}>
+      <p className="id">{id ? id : newPuppyId}</p>
+      {/*//todo upload <img />*/}
+      <label>
+        Name:{" "}
+        <input
+          name="name"
+          placeholder="Puppy Name"
+          type="text"
+          defaultValue={name ? name : ""}
+          ref={register()}
+        />
+      </label>
+      <label>
+        Microchip #:{" "}
+        <input
+          name="microchipId"
+          placeholder="#######"
+          type="text"
+          defaultValue={microchipId ? microchipId : ""}
+          ref={register()}
+        />
+        <select
+          name="sex"
+          defaultValue={sex ? sex : "M"}
+          ref={register({ required: false })}
+        >
+          <option value="M">Male</option>
+          <option value="F">Female</option>
+        </select>
+      </label>
+      <label>
+        Markings:{" "}
+        <input
+          // todo choose multiple markings array
+          name="markings"
+          placeholder="markings"
+          defaultValue={markings ? markings : ""}
+          type="text"
+          list="markings"
+          ref={register({ required: false })}
+        />
+        <datalist id="markings">
+          <option>Cream</option>
+          <option>Chocolate</option>
+          <option>Dapple</option>
+        </datalist>
+      </label>
+      <div className="new-owner">
+        {/*
+        //todo add new owner/choose existing owner (new form)
+         */}
+        <label>
+          First Name:{" "}
+          <input
+            name="firstName"
+            placeholder="First Name"
+            defaultValue={firstName ? firstName : ""}
+            type="text"
+            ref={register({ required: false })}
+          />
+        </label>
+        <label>
+          Last Name:{" "}
+          <input
+            name="lastName"
+            placeholder="Last Name"
+            defaultValue={lastName ? lastName : ""}
+            type="text"
+            ref={register({ required: false })}
+          />
+        </label>
+        <label>
+          Phone:{" "}
+          <input
+            name="phone"
+            placeholder="Phone #"
+            defaultValue={phone ? phone : ""}
+            type="text"
+            ref={register({ required: false })}
+          />
+        </label>
+        <label>
+          Address: {/* //todo split this into further breakdown */}
+          <input
+            name="address"
+            placeholder="Address"
+            defaultValue={address ? address : ""}
+            type="text"
+            ref={register({ required: false })}
+          />
+        </label>
+      </div>
+      <label>
+        Price:{" "}
+        <input
+          name="price"
+          placeholder="$$"
+          defaultValue={price ? price : ""}
+          type="text"
+          ref={register({ required: false })}
+        />
+      </label>
+      {/* //todo validate number  */}
+      <div>
+        <label>
+          Payment:{" "}
+          <input
+            name="paymentValue"
+            type="text"
+            defaultValue={paymentValue ? paymentValue : ""}
+            placeholder="$"
+            ref={register({ required: false })}
+          />
+        </label>
+        <label>
+          Payment Type:{" "}
+          <input
+            name="paymentType"
+            type="text"
+            list="paymentType"
+            defaultValue={paymentType ? paymentType : "Cash"}
+            // default="cash"
+            ref={register({ required: false })}
+          />
+          <datalist id="paymentType">
+            <option>Cash</option>
+            <option>Check</option>
+            <option>Card</option>
+            <option>Venmo</option>
+            <option>PayPal</option>
+          </datalist>
+        </label>
+      </div>
+      <label>
+        Fees:{" "}
+        <input
+          name="fees"
+          type="text"
+          placeholder="$"
+          defaultValue={fees ? fees : ""}
+          ref={register({ required: false })}
+        />
+      </label>
+      <label>
+        Seller Payment:{" "}
+        <input
+          name="sellerPayment"
+          type="text"
+          defaultValue={sellerPayment ? sellerPayment : ""}
+          placeholder="$"
+          ref={register({ required: false })}
+        />
+      </label>
+      <label>
+        AKC:{" "}
+        <input
+          name="akcRegistered"
+          type="checkbox"
+          defaultValue={akcRegistered ? firstName : false}
+          ref={register({ required: false })}
+        />
+      </label>
+      <label>
+        Listed:{" "}
+        <input
+          name="listed"
+          type="checkbox"
+          defaultValue={listed ? listed : false}
+          ref={register({ required: false })}
+        />
+      </label>
+      <div className="actions">
+        <button onClick={cancel}>Cancel</button>
+        {editingPuppy && (
+          <button onClick={() => RemovePuppy(editingPuppy.id)}>Delete</button>
+        )}
+        <input type="submit" value="Submit Puppy!" />
+      </div>
+    </form>
+  );
+
+  function CommissionRate() {
+    // "0-799": "10%",
+    // "800-1599": "15%",
+    // "1600": "20%"
+    const price = watch("price");
+    if (!price) return 0;
+
+    if (price < 799) {
+      return price * 0.1;
+    } else if (price < 1599) {
+      return price * 0.15;
+    } else {
+      return price * 0.2;
+    }
+  }
+}
